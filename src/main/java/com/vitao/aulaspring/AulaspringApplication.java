772975af1187bf059fc07498029767040061d42e
Victor Oliveira <vimatozin@hotmail.com>
2021-04-24 10:05:28 -0300

    Primeiro commit do projeto spring

---
+++ src/main/java/com/vitao/aulaspring/AulaspringApplication.java
@@ -0,0 +1,13 @@
+package com.vitao.aulaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitao/aulaspring/AulaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitao.aulaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
