b4aa15aa8b63196a8a3ba5adb8bb49f6315d12b5
Victor Oliveira <vimatozin@hotmail.com>
2021-11-29 22:08:54 -0300

    config e envio pra prod

--- src/main/java/com/vitao/aulaspring/config/JacksonConfig.java
+++ src/main/java/com/vitao/aulaspring/config/JacksonConfig.java
@@ -12,14 +12,14 @@ public class JacksonConfig {
 // https://stackoverflow.com/questions/41452598/overcome-can-not-construct-instance-of-interfaceclass-without-hinting-the-pare
     @Bean
     public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder() {
+            @Override
             public void configure(ObjectMapper objectMapper) {
                 objectMapper.registerSubtypes(PagamentoComCartao.class);
                 objectMapper.registerSubtypes(PagamentoComBoleto.class);
                 super.configure(objectMapper);
             }
         };
-        return builder;
     }
 }
 //Basicamente uma classe de configuração jackson permite que usando a anotação @configuration
